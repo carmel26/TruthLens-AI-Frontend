4b3cfdda13488ebbecda686cae83f49a987f893e
carmel26 <nkeshipelagie@gmail.com>
2026-07-28 18:19:15 +0300

    adding online link for production

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: '/api/v1',
-  appName: 'TruthLens AI',
-  appTagline: 'Before you trust it, check it.',
-  version: '1.0.0-phase1',
+  apiUrl: "http://158.176.2.130/api/v1",
+  appName: "TruthLens AI",
+  appTagline: "Before you trust it, check it.",
+  version: "1.0.0-phase1",
 };
