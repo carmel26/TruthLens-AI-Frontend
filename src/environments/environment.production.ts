5acd58e92d850814ebea53fa9a2853e11a0203bf
carmel26 <nkeshipelagie@gmail.com>
2026-07-29 17:09:47 +0300

    adding ssl and redirections

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: "http://158.176.2.130/api/v1",
+  apiUrl: "https://158.176.2.130.sslip.io/api/v1",
   appName: "TruthLens AI",
   appTagline: "Before you trust it, check it.",
   version: "1.0.0-phase1",
